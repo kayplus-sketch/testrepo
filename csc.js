e2976b129cdaed103def1141b9c7de01d9f24192
Kuam kolawole dada <kuamd71@gmail.com>
2024-12-19 23:19:29 -0800

    csc new page

---
+++ csc.js
@@ -0,0 +1,26 @@
+let hello = " Hello word ";
+
+let message;
+
+message = hello;
+
+alert(hello); 
+
+let year = prompt(
+  "In which year was ECMAScript-2015 specification published?",
+  ""
+);
+
+if (year == 4445) alert("You are right!");
+
+let message1 = "hello";
+message1 = 123456;
+alert('kolawole');
+
+let message2 = "hello";
+message1 = 123456;
+alert();
+
+let age = prompt("How old are you?", 100);
+
+alert(`You are ${age} years old!`);
